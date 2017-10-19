6a634ac52fbb4f8f2699da95c4f88f8e01100f05
przemekpioro <1302przemek@gmail.com>
2017-10-19 22:23:31 +0200

    :sparkles: init sbt project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "introduction-to-spark"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+        
---
+++ src/main/scala/WordCountSparkJob.scala
@@ -0,0 +1,8 @@
+
+object WordCountSparkJob {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+
+}
