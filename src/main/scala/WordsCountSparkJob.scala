ac5c33d3e15e420b2a22a5e6d54a487401893c7f
przemekpioro <1302przemek@gmail.com>
2017-10-20 10:59:56 +0200

    :sparkles: word count spark job

--- src/main/scala/WordsCountSparkJob.scala
+++ src/main/scala/WordsCountSparkJob.scala
@@ -6,17 +6,15 @@ object WordsCountSparkJob {
     implicit val sparkSession = SparkSession.builder().appName("Word Count").master("local").getOrCreate()
     import sparkSession.implicits._
 
-    val words = sparkSession.createDataset(Seq("Spark", "asda spark", "spark", "word", "count"))
-
-    words.printSchema()
+    val words = sparkSession.createDataset(Seq("Spark", "apache spark", "spark", "word", "count"))
 
     val counter = words
-      .flatMap(_.split(","))
-      .map(word => word.toLowerCase)
+      .flatMap(_.split(" "))
+      .map(word => word.toLowerCase.trim)
       .groupByKey(value => value)
       .count()
 
-    counter.write.option("header","true")csv("/Users/przemekpioro/siftlogic/introduction-to-spark/csv")
+    counter.show(10)
 
   }
 
